ecbc6e7aa61ea5b3f8a94c3a525798c360dde760
Tarun Namdev <namdevtarun910@gmail.com>
2025-01-17 14:20:36 +0530

    changed title

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,9 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "namdev-patrika",
-  description: "Namdev Patrika is a platform that connects people from the Namdev Samaj community to help them find a life partner. We focus on matching individuals based on shared values, traditions, and cultural backgrounds. We understand how important it is to find someone who not only suits your personality but also connects with your community's values. Our easy-to-use matchmaking process is designed to help you find a meaningful connection and a partner who shares your culture and traditions, making the search for a life partner simpler and more rewarding.",
+  title: "युवक युवती परिचय सम्मेलन 2025",
+  description:
+    "Namdev Patrika is a platform that connects people from the Namdev Samaj community to help them find a life partner. We focus on matching individuals based on shared values, traditions, and cultural backgrounds. We understand how important it is to find someone who not only suits your personality but also connects with your community's values. Our easy-to-use matchmaking process is designed to help you find a meaningful connection and a partner who shares your culture and traditions, making the search for a life partner simpler and more rewarding.",
 };
 
 export default function RootLayout({ children }) {
@@ -22,9 +23,9 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           <Navbar />
-          
+
           {children}
-          <Footer/>
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
